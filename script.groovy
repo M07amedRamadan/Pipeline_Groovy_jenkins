0ee9ee283ffd80b527a18db73f48fd26bb151769
M0hamedRamadan <67959462+M0hamedRamadan@users.noreply.github.com>
2023-07-27 00:45:48 +0300

    Create script used in pipeline file to perform jobs

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildApp() {
+    echo 'this job to create app using script.'
+}
+
+def deployApp() {
+    echo 'this job to deploy app using script.'
+}
+
+def testApp() {
+    echo 'this job to test app using script.'
+}
+
+return this
